28d86a7da12505734b305d955ef92a3a2ebad504
duynguyen-dev <vanduy19082013@gmail.com>
2017-10-06 11:12:40 +0700

    Xong connect send

--- src/app.js
+++ src/app.js
@@ -1,7 +1,23 @@
 const openStream = require('./openStream');
+const $ = require('jquery');
 
-openStream();
+//openStream();
 
 const Peer = require('simple-peer');
 
-const P = new Peer({ initiator });
+const p = new Peer({ initiator: location.hash === '#1', trickle: false });
+
+p.on('signal', token => {
+    $('#txtMySignal').val(JSON.stringify(token));
+});
+
+$('#btnConnect').click(() => {
+    const friendSignal = JSON.parse($('#txtFriendSignal').val());
+    p.signal(friendSignal);
+});
+
+p.on('connect', () => {
+    setInterval(() => p.send(Math.random()), 2000);
+});
+
+p.on('data', data => console.log(data));
